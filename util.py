33e7d3e6b4efb468c97318f5077b526cddfa88fc
Charles Liu <therealcharlesliu@gmail.com>
2015-09-09 22:27:10 -0400

    add code

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 ##### Filename: util.py
-##### Author: {your name}
-##### Date: {current date}
-##### Email: {your email}
+##### Author: Charles Liu
+##### Date: 9/8/2015
+##### Email: cliu02@g.harvard.edu
 
 import copy
 from collections import deque
@@ -9,56 +9,84 @@ from collections import deque
 ## Problem 1
 
 def matrix_multiply(x, y):
-    pass
+    #return matrix dimensions are len(x) x len(y[0])
+    ret = [[]]*len(x)
+    #loop through rows of x/columns of y
+    for row in range(0, len(x)):
+        #loop through cols of y
+        for col in range(0, len(y[0])):
+            sum = 0
+            #sumproduct of row and col
+            for index in range(0, len(x[row])):
+                sum += x[row][index]*y[index][col]
+            ret[row] = ret[row] + [sum]
+    return ret
+        
 
 ## Problem 2, 3
 
 class MyQueue:
     def __init__(self):
-        pass
+        self._deq = deque()
     def push(self, val):
-        pass
+        self._deq.append(val)
     def pop(self):
-        pass
+        if self._deq.count == 0:
+            return None
+        return self._deq.popleft()
     def __eq__(self, other):
-        pass
+        return self._deq == other._deq if isinstance(other, MyQueue) else False
     def __ne__(self, other):
-        pass
+        return ((self == other) == False)
     def __str__(self):
-        pass
+        return str(self._deq).replace('deque', 'myqueue', 1)
 
 class MyStack:
     def __init__(self):
-        pass
+        self._deq = deque()
     def push(self, val):
-        pass
+        self._deq.append(val)
     def pop(self):
-        pass
+        if self._deq.count == 0:
+            return None
+        return self._deq.pop()
     def __eq__(self, other):
-        pass
+        return self._deq == other._deq if isinstance(other, MyStack) else False
     def __ne__(self, other):
-        pass
+        return ((self == other) == False)
     def __str__(self):
-        pass
+        return str(self._deq).replace('deque', 'mystack', 1)
 
 ## Problem 4
 
 def add_position_iter(lst, number_from=0):
-    pass
+    ret = copy.copy(lst)
+    count = 0
+    for i in range(0, len(ret)):
+        ret[i] += number_from + count
+        count += 1
+    return ret
+        
 
 def add_position_recur(lst, number_from=0):
-    pass
+    if len(lst) == 0:
+        return []
+    removed_el = copy.copy(lst)
+    del removed_el[0]
+    return [lst[0] + number_from] + add_position_recur(removed_el, number_from + 1) 
 
 def add_position_map(lst, number_from=0):
-    pass
+    return map(lambda (i,x): i + x + number_from, enumerate(lst))
 
 ## Problem 5
 
 def remove_course(roster, student, course):
-    pass
+    roster[student] = roster[student] - set([course])
+    return roster
 
 ## Problem 6
 
 def copy_remove_course(roster, student, course):
-    pass
+    roster_new = copy.copy(roster)
+    return remove_course(roster_new, student, course)
 
